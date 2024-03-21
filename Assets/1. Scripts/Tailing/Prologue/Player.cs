39c7f392b16d9bbc3997cf0548956cc5a960ab02
ollan365 <80192257+ollan365@users.noreply.github.com>
2024-03-22 03:46:23 +0900

    #1. Merge StartLogic File

--- Assets/1. Scripts/Tailing/Prologue/Player.cs
+++ Assets/1. Scripts/Tailing/Prologue/Player.cs
@@ -1,65 +1,33 @@
 using UnityEngine;
-using System.Collections.Generic;
-using TMPro;
+using System;
 public class Player : MonoBehaviour
 {
-    [SerializeField] private string playerName;
-    [SerializeField] private TMP_InputField nameInput;
-    [SerializeField] private TextMeshProUGUI nameCheckQuestion;
-    [SerializeField] private TMP_Dropdown monthDropdown, dayDropdown;
-    public string Name
+    public static Player Instance { get; private set; }
+    private void Awake()
     {
-        get => playerName;
-        set
+        if (Instance == null)
         {
-            playerName = nameInput.text == "" ? "필연" : nameInput.text;
-            nameCheckQuestion.text = $"\"{playerName}\"으로 확정하시겠습니까?";
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
         }
     }
-    private int language = 1;
-    public int Language
-    {
-        get => language;
-        set => language = value;
-    }
-    private int gender;
-    public int Gender
-    {
-        get => gender;
-        set
-        {
-            gender = value;
-            if (language == 2 && gender == 1) language++;
-        }
-    }
-    private int month, day;
-    public int Month { get => month; }
-    public int Day { get => day; }
-    public void ChangeDayOption()
-    {
-        List<int> optionList = new();
-        for (int i = 1; i <= 29; i++)
-            optionList.Add(i);
+    
+    private string playerName;
+    public string Name { get => playerName; set => playerName = value; }
 
-        switch (monthDropdown.value + 1)
-        {
-            case 2:
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                optionList.Add(30);
-                break;
-            default:
-                optionList.Add(30);
-                optionList.Add(31);
-                break;
-        }
-    }
-    public void SetBirth()
-    {
-        month = monthDropdown.value + 1;
-        day = dayDropdown.value + 1;
-    }
+    private int language = 1;
+    public int Language { get => language; set => language = value; }
+
+    private int gender;
+    public int Gender { get => gender; set => gender = value; }
+    private int month, day;
+    public int Month { get => month; set => month = value; }
+    public int Day { get => day; set => day = value; }
+
+    private int actPower;
+    public int ActPower { get => actPower; set => actPower = Math.Clamp(value, 0, 15); }
 }
--- Assets/1. Scripts/Tailing/Prologue/StartLogic.cs
+++ Assets/1. Scripts/Tailing/Prologue/StartLogic.cs
@@ -1,15 +1,50 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 using TMPro;
 public class StartLogic : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI text, choiceTextA, choiceTextB;
-    [SerializeField] private GameObject[] choiceBTNs;
-    [SerializeField] private Button[] prologueBTN;
-
+    [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private TextMeshProUGUI nameCheckQuestion;
+    [SerializeField] private TMP_Dropdown monthDropdown, dayDropdown;
     public void GoScene(int sceneNum)
     {
         SceneManager.LoadScene(sceneNum);
+    }
+
+    public void SetName()
+    {
+        Player.Instance.Name = nameInput.text == "" ? "필연" : nameInput.text;
+        nameCheckQuestion.text = $"\"{Player.Instance.Name}\"으로 확정하시겠습니까?";
+    }
+
+    public void ChangeDayOption()
+    {
+        List<TMP_Dropdown.OptionData> optionList = new();
+        for (int i = 1; i <= 29; i++)
+            optionList.Add(new TMP_Dropdown.OptionData(i.ToString()));
+
+        switch (monthDropdown.value + 1)
+        {
+            case 2:
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                optionList.Add(new TMP_Dropdown.OptionData(30.ToString()));
+                break;
+            default:
+                optionList.Add(new TMP_Dropdown.OptionData(30.ToString()));
+                optionList.Add(new TMP_Dropdown.OptionData(31.ToString()));
+                break;
+        }
+        dayDropdown.ClearOptions();
+        dayDropdown.AddOptions(optionList);
+    }
+    public void SetBirth()
+    {
+        Player.Instance.Month = monthDropdown.value + 1;
+        Player.Instance.Day = dayDropdown.value + 1;
     }
 }
