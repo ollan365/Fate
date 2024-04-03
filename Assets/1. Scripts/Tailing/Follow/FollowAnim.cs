0b2712f5d27e90d8d64278726ac32896b5ca3a02
ollan365 <80192257+ollan365@users.noreply.github.com>
2024-04-04 00:37:00 +0900

    Push Follow Image
    미행 관련 이미지들을 넣고 폴더에 정리

--- Assets/1. Scripts/Tailing/Follow/FollowAnim.cs
+++ Assets/1. Scripts/Tailing/Follow/FollowAnim.cs
@@ -2,48 +2,45 @@ using UnityEngine;
 
 public class FollowAnim : MonoBehaviour
 {
-	//[Header("배경")]
-	//[SerializeField] private Transform[] positions;
-	//[SerializeField] private float moveSpeed;
+    [Header("배경")]
+    [SerializeField] private Transform backgroundPosition;
+    [SerializeField] private float moveSpeed;
 
-	//[Header("캐릭터")]
-	//[SerializeField] private Player player;
- //   [SerializeField] private Animator fateBoy, fateGirl, accidyBoy, accidyGirl;
-	//private Animator fate, accidy;
+    //[Header("캐릭터")]
+    //[SerializeField] private Player player;
+    //   [SerializeField] private Animator fateBoy, fateGirl, accidyBoy, accidyGirl;
+    //private Animator fate, accidy;
 
-	//private bool isStop;
-	//public bool IsStop
-	//{
-	//	set
-	//	{
-	//		isStop = !isStop;
-	//		fate.SetBool("Walking", !isStop);
-	//		accidy.SetBool("Walking", !isStop);
-	//	}
-	//}
- //   private void Start()
- //   {
-	//	isStop = true;
-	//	SetCharcter(1);
- //   }
- //   private void Update()
-	//{
-	//	if (!isStop)
-	//	{
-	//		foreach (Transform t in positions)
-	//			t.position += Vector3.left * moveSpeed * Time.deltaTime;
-	//	}
-	//}
+    private bool isStop = true;
+    public bool IsStop
+    {
+        set
+        {
+            isStop = !isStop;
+            // fate.SetBool("Walking", !isStop);
+            // accidy.SetBool("Walking", !isStop);
+        }
+    }
+    private void Start()
+    {
+        // SetCharcter(1);
+    }
+    private void Update()
+    {
+        if (!isStop)
+            backgroundPosition.position += Vector3.left * moveSpeed * Time.deltaTime;
+        
+    }
 
-	//public void SetCharcter(int accidyGender)
- //   {
-	//	if (player.FateGender == 0) fate = fateBoy;
-	//	else fate = fateGirl;
+    //public void SetCharcter(int accidyGender)
+    //   {
+    //	if (player.FateGender == 0) fate = fateBoy;
+    //	else fate = fateGirl;
 
-	//	if (accidyGender == 0) accidy = accidyBoy;
-	//	else accidy = accidyGirl;
+    //	if (accidyGender == 0) accidy = accidyBoy;
+    //	else accidy = accidyGirl;
 
-	//	fate.gameObject.SetActive(true);
-	//	accidy.gameObject.SetActive(true);
-	//}
+    //	fate.gameObject.SetActive(true);
+    //	accidy.gameObject.SetActive(true);
+    //}
 }
