fb8dc2f64e787ef42d4ef52032edcebed0e2c195
fivestar1103 <fivestar1103@uos.ac.kr>
2024-04-12 18:42:10 +0900

    RoomManager에서 ImageAndLockPanelManager 분리 등
    1. OpenDoors와 CloseDoors를 Doors 스크립트 하나로 통합함
    
    2. DialogueManager에서 Choice 클릭 시 dialogueQueue 관련 문제를 해결함 (EndDialogue를 곳곳에 추가)
    
    3. ImageAndLockPanelManager 관련
     - 오브젝트의 이미지를 확대하는 기능과 잠금 장치(Lock Objects)들을 활성화하는 기능을 RoomManager에서 분리함
     - Blocking Panel을 사용하여 조사중이거나 잠금장치 활성화 시에 다른 것들이 클릭 불가능하도록 함 (기획 의도대로 살짝 어둡게 설정)
     - 이 과정에서 RoomManager를 깔끔하게 정리함
    
    4. DialogueParser가 "`"(백틱)을 ","(콤마)로 바꾸도록 수정함 (CSV파일의 특성상 대사에 콤마를 넣기 어려우므로 백틱을 쓰기로 결정)
    
    5. choice의 레이어가 잘못되어 있어서 캐릭터 이미지 뒤에 있던 것을 고침
    
    6. exit button UI 리소스 추가하고 위치를 화면 하단으로 변경함

--- Assets/1. Scripts/Managers/RoomManager.cs
+++ Assets/1. Scripts/Managers/RoomManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +10,11 @@ public class RoomManager : MonoBehaviour
 {
     public static RoomManager Instance { get; private set; }
     
-    private GameObject currentView;  // 현재 뷰
     [Header("시점들")][SerializeField] private List<GameObject> sides;  // 시점들
+    private GameObject currentView;  // 현재 뷰
     private int currentSideIndex = 0;  // 현재 시점 인덱스
-
-    [Header("확대 화면들")][SerializeField] private List<GameObject> zoomViews;  // 확대 화면들
     
-    // 조사 중이거나 확대 중이면 이동키로 시점 바꾸지 못하게 함
-    public bool isInvestigating = false;
-    public bool isZoomed = false;
-
-    private List<GameObject> screenObjects = new List<GameObject>();
-
-    // 다이얼로그 매니저의 isDialogueActive가 true면 다른 버튼들 비활성화시킴
-    private Button[] otherButtons;
+    [Header("확대 화면들")][SerializeField] private List<GameObject> zoomViews;  // 확대 화면들
 
     // 이동 버튼
     [Header("이동 버튼들")] 
@@ -32,18 +24,12 @@ public class RoomManager : MonoBehaviour
     // 나가기 버튼
     [Header("나가기 버튼")] [SerializeField] private Button exitButton;
 
-    // 이벤트오브젝트패널
-    [Header("이벤트 오브젝트 패널")] [SerializeField] private GameObject eventObjectPanel;
+    // 이벤트 오브젝트 패널 매니저
+    [FormerlySerializedAs("eventObjectPanelManager")] public ImageAndLockPanelManager imageAndLockPanelManager;
+    // 조사 중이거나 확대 중이면 이동키로 시점 바꾸지 못하게 함
+    [SerializeField] private bool isInvestigating = false;
+    [SerializeField] private bool isZoomed = false;
     
-    [Header("이벤트 오브젝트 확대 이미지")]
-    [SerializeField] private GameObject amuletImage;
-    [SerializeField] private GameObject carpetPaperImage;
-    [SerializeField] private GameObject clockImage;
-    [SerializeField] private GameObject keysImage;
-    [SerializeField] private GameObject knifeImage;
-    [SerializeField] private GameObject posterImage;
-    [SerializeField] private GameObject liquorImage;
-
     void Awake()
     {
         if (Instance == null)
@@ -58,9 +44,6 @@ public class RoomManager : MonoBehaviour
     
     void Start()
     {
-        // "Controlled Button" 태그를 가진 모든 버튼들을 찾아서 otherButtons에 넣음.
-        otherButtons = GameObject.FindGameObjectsWithTag("Controlled Button").Select(g => g.GetComponent<Button>()).ToArray();
-
         // 모든 Side 켰다 끄기
         foreach (GameObject side in sides)
         {
@@ -74,109 +57,55 @@ public class RoomManager : MonoBehaviour
             zoomView.SetActive(true);
             zoomView.SetActive(false);
         }
-        
+
         // Side 1으로 초기화
         currentView = sides[0];
         SetCurrentSide(0);
-        
+
         DialogueManager.Instance.StartDialogue("Prologue_015");
     }
 
-    // A키와 D키로 시점 이동
-    void Update()
+    public void MoveSides(int leftOrRight)  // left: -1, right: 1
     {
-        //if (isInvestigating || DialogueManager.Instance.isDialogueActive) return; 
-        //if (Input.GetKeyDown(KeyCode.A))
-        //{
-        //    int newSideIndex = (currentSideIndex - 1 + sides.Count) % sides.Count;
-        //    SetCurrentSide(newSideIndex);
-        //}
-        //else if (Input.GetKeyDown(KeyCode.D))
-        //{
-        //    int newSideIndex = (currentSideIndex + 1) % sides.Count;
-        //    SetCurrentSide(newSideIndex);
-        //}
-
-        // 조사/다이얼로그 출력을 안 하고 있을 때만 이동 버튼이 보임.
-        if (isInvestigating || DialogueManager.Instance.isDialogueActive)
-            SetMoveButton(false);
-        else
-            SetMoveButton(true);
-    }
-
-    // 왼쪽 이동 버튼
-    public void MoveLeftButton()
-    {
-        if (isInvestigating || DialogueManager.Instance.isDialogueActive) return;
-        else
+        if (leftOrRight != -1 && leftOrRight != 1)
         {
-            int newSideIndex = (currentSideIndex - 1 + sides.Count) % sides.Count;
-            SetCurrentSide(newSideIndex);
+            Debug.Log("Value of leftOrRight must be -1 or 1!");
+            return;
         }
+
+        int newSideIndex = (currentSideIndex + sides.Count + leftOrRight) % sides.Count;
+        SetCurrentSide(newSideIndex);
     }
 
-    // 오른쪽 이동 버튼
-    public void MoveRightButton()
-    {
-        if (isInvestigating || DialogueManager.Instance.isDialogueActive) return;
-        else
-        {
-            int newSideIndex = (currentSideIndex + 1) % sides.Count;
-            SetCurrentSide(newSideIndex);
-        }
-    }
-
-    // 이동 버튼들이 조사/다이얼로그 출력 시에는 화면 상에서 보이지 않게 함
-    public void SetMoveButton(bool isTrue)
-    {
-        moveButtonLeft.gameObject.SetActive(isTrue);
-        moveButtonRight.gameObject.SetActive(isTrue);
-    }
-
-
-    public void ControllEventButtons(bool isTrue)
-    {
-        foreach (Button button in otherButtons)
-        {
-            button.interactable = isTrue;
-        }
-    }
-
-    public void SearchExitButton()
+    public void OnExitButtonClick()
     {
         if (isInvestigating)
         {
-            DeactivateObjects();
-            isInvestigating = false;
+            imageAndLockPanelManager.OnExitButtonClick();
+            
+            SetButtons();
+            
+            return;
         }
-        else if (isZoomed)
+        
+        if (isZoomed)
         {
             SetCurrentView(sides[currentSideIndex]);
             isZoomed = false;
+            
+            SetButtons();
         }
-        
-        if (!isInvestigating && !isZoomed) exitButton.gameObject.SetActive(false); 
-    }
-
-    private void DeactivateObjects()
-    {
-        foreach (GameObject obj in screenObjects)
-        {
-            obj.SetActive(false);
-        }
-
-        //ScreenObjects.Clear();
-    }
-
-    public void AddScreenObjects(GameObject obj)
-    {
-        if (!screenObjects.Contains(obj)) screenObjects.Add(obj);
     }
 
     // ######################################## setters ########################################
     public void SetIsInvestigating(bool isTrue)
     {
         isInvestigating = isTrue;
+    }
+    
+    public void SetIsZoomed(bool isTrue)
+    {
+        isZoomed = isTrue;
     }
 
     // 시점 전환
@@ -192,60 +121,28 @@ public class RoomManager : MonoBehaviour
         currentView.SetActive(false);
         newView.SetActive(true);
         currentView = newView;
-        
-        if (!sides.Contains(newView))  // side 중 하나가 아니라면 확대중인 화면이다
-        {
-            SetExitButton(true);
-            isZoomed = true;
-        }
     }
-
+    
     // 나가기 버튼 필요 시, 보이게 함 (ResultManager에서 호출하게 함)
     public void SetExitButton(bool isTrue)
     {
         exitButton.gameObject.SetActive(isTrue);
     }
-
-    // EventObjectPanel 켜서 해당 오브젝트 확대 UI 보여줌
-    public void SetEventObjectPanel(bool isTrue, string objName)
+    
+    // 이동 버튼들이 조사/다이얼로그 출력 시에는 화면 상에서 보이지 않게 함
+    private void SetMoveButtons(bool isTrue)
     {
-        eventObjectPanel.SetActive(isTrue);
-        if (eventObjectPanel.activeSelf)
-        {
-            AddScreenObjects(eventObjectPanel);
-            SetExitButton(true);
-        }
-        switch (objName)
-        {
-            case "Pillow":
-                amuletImage.SetActive(isTrue);
-                AddScreenObjects(amuletImage);
-                break;
-            case "Carpet_Paper":
-                carpetPaperImage.SetActive(isTrue);
-                AddScreenObjects(carpetPaperImage);
-                break;
-            case "clock1":
-                clockImage.SetActive(isTrue);
-                AddScreenObjects(clockImage);
-                break;
-            case "ClockKeys":
-                keysImage.SetActive(isTrue);
-                AddScreenObjects(keysImage);
-                break;
-            case "Knife":
-                knifeImage.SetActive(isTrue);
-                AddScreenObjects(knifeImage);
-                break;
-            case "Poster":
-                posterImage.SetActive(isTrue);
-                AddScreenObjects(posterImage);
-                break;
-            case "Liquor":
-                liquorImage.SetActive(isTrue);
-                AddScreenObjects(liquorImage);
-                break;
-        }
+        moveButtonLeft.gameObject.SetActive(isTrue);
+        moveButtonRight.gameObject.SetActive(isTrue);
     }
 
+    public void SetButtons()
+    {
+        bool isInvestigatingOrZoomed = isInvestigating || isZoomed;
+        bool isDialogueActive = DialogueManager.Instance.isDialogueActive;
+        
+        SetExitButton(isInvestigatingOrZoomed && !isDialogueActive);
+        SetMoveButtons(!(isInvestigatingOrZoomed || isDialogueActive));
+    }
+    
 }
