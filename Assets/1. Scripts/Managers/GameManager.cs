48c4cda8c5b4d489b742caebe124d7e803c0d379
Jieun <jieundino@naver.com>
2024-05-03 19:49:51 +0900

    0501 중간 커밋
    #행동력 시스템 표기 하기
    ActionPointManager 스크립트를 만들고 GameManager에 행동력 관련 이벤트 추가함
    #사이드2번방 의자 앞에 이미지 추가

--- Assets/1. Scripts/Managers/GameManager.cs
+++ Assets/1. Scripts/Managers/GameManager.cs
@@ -22,6 +22,10 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI variablesText;
     public bool isDebug = false;
 
+    // 행동력 변수 변경될 때 호출될 이벤트
+    public delegate void ActionPointChangedEventHandler(int newActionPointValue);
+    public static event ActionPointChangedEventHandler OnActionPointChanged;
+
     void Awake()
     {
         if (Instance == null)
@@ -230,7 +234,11 @@ public class GameManager : MonoBehaviour
         int cnt = (int)GetVariable(variableName);
         cnt--;
         SetVariable(variableName, cnt);
-        
+
+        // 행동력 감소 시 행동력 감소함을 이벤트로 알림
+        if (variableName == "ActionPoint")
+            OnActionPointChanged?.Invoke(cnt);
+
         if (isDebug) ShowVariables();
     }
 
@@ -239,6 +247,10 @@ public class GameManager : MonoBehaviour
         int cnt = (int)GetVariable(variableName);
         cnt -= count;
         SetVariable(variableName, cnt);
+
+        // 행동력 감소 시 행동력 감소함을 이벤트로 알림
+        if (variableName == "ActionPoint")
+            OnActionPointChanged?.Invoke(cnt);
 
         if (isDebug) ShowVariables();
     }
