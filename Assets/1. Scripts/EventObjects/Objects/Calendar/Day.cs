60aecd1cbe799e1117a21aa1880bc7f194961c5f
fivestar1103 <fivestar1103@uos.ac.kr>
2024-05-11 04:47:27 +0900

    Calendar Complete
    - Day.cs에서 유저가 선택 가능한 날짜인 필연의 생일과 특별한 날짜가 겹치는 경우 예외 처리를 하는 것에 큰 어려움을 겪음
    - 겹칠때와 겹치지 않을 때의 이벤트를 따로 정의하였음
    - 최대한 가독성을 잃지 않기 위해 노력하였음.....

--- Assets/1. Scripts/EventObjects/Objects/Calendar/Day.cs
+++ Assets/1. Scripts/EventObjects/Objects/Calendar/Day.cs
@@ -1,43 +1,72 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Day: MonoBehaviour
+public class Day : MonoBehaviour
 {
-    public void OnMouseDown()
+    private Dictionary<string, string> specialDates = new Dictionary<string, string>();
+    private bool sameSpecialDate = false;
+    private string date;
+    
+    void Start()
     {
+        CheckSpecialDate();
+    }
+
+    private void CheckSpecialDate()
+    {
+        string day = GetComponentInChildren<TextMeshProUGUI>().text.PadLeft(2, '0');
         int month = (int)GameManager.Instance.GetVariable("CalendarMonth");
-        string monthString = month.ToString();
-        if (month < 10) monthString = "0" + monthString;
+        string monthString = month.ToString().PadLeft(2, '0');
 
-        string day = GetComponentInChildren<TextMeshProUGUI>().text;
-        if (day.Length == 1) day = "0" + day;
-
-        string date = monthString + day;
-        
-        Debug.Log(date);
+        date = monthString + day;
 
         string fateBirthday = (string)GameManager.Instance.GetVariable("FateBirthday");
         string accidyBirthday = (string)GameManager.Instance.GetVariable("AccidyBirthday");
+        
+        specialDates.Add("FateBirthday", fateBirthday);
+        specialDates.Add("AccidyBirthday", accidyBirthday);
+        specialDates.Add("1031", "1031");
+        specialDates.Add("1001", "1001");
 
-        if (date == fateBirthday)
+        if (specialDates["FateBirthday"] == specialDates["AccidyBirthday"] ||
+            specialDates["FateBirthday"] == "1031" ||
+            specialDates["FateBirthday"] == "1001")
         {
-            EventManager.Instance.CallEvent("EventStorageCalendarFateBirth");
+            sameSpecialDate = true;
         }
-        else if (date == accidyBirthday)
+
+        if (specialDates.ContainsValue(date))
         {
-            EventManager.Instance.CallEvent("EventStorageCalendarAccidyBirth");
+            GetComponent<Image>().color = Color.magenta;
         }
-        else if (date == "1031")
+
+    }
+
+    public void OnMouseDown()
+    {
+        string eventID = null;
+
+        if (date == specialDates["FateBirthday"] && !sameSpecialDate)
         {
-            EventManager.Instance.CallEvent("EventStorageCalendar1031");
+            eventID = "EventCalendarBirthdayFate";
         }
-        else if (date == "1001")
+        if (date == specialDates["AccidyBirthday"])
         {
-            EventManager.Instance.CallEvent("EventStorageCalendar1001");
+            eventID = specialDates["FateBirthday"] == specialDates["AccidyBirthday"] ? "EventCalendarSameBirthdays" : "EventCalendarBirthdayAccidy";
         }
+        if (date == "1031")
+        {
+            eventID = specialDates["FateBirthday"] == "1031" ? "EventCalendarSameAsSpecialDateA" : "EventCalendarSpecialDateA"; 
+        }
+        if (date == "1001")
+        {
+            eventID = specialDates["FateBirthday"] == "1001" ? "EventCalendarSameAsSpecialDateB" : "EventCalendarSpecialDateB"; 
+        }
+
+        if (string.IsNullOrEmpty(eventID)) return;
+        EventManager.Instance.CallEvent(eventID);
     }
 }
