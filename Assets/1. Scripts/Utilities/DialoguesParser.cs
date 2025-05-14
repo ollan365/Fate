7160acd9b6c8800370261a4eebd09fce7c42e762
Jieun <jieundino@naver.com>
2025-05-14 21:17:24 +0900

    Update DialoguesParser.cs

--- Assets/1. Scripts/Utilities/DialoguesParser.cs
+++ Assets/1. Scripts/Utilities/DialoguesParser.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DialoguesParser : MonoBehaviour
+public class DialoguesParser
 {
     // CSV 파일
     private TextAsset dialoguesCSV = Resources.Load<TextAsset>("Datas/dialogues");
