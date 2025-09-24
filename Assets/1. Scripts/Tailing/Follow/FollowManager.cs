2bc7cd3f4c22f72d2b3d7e431c2b8bfddbdd2734
Jieun <jieundino@naver.com>
2025-09-24 23:43:09 +0900

    Update FollowManager.cs
    방탈출 공통 UI 비활성화 코드 추가

--- Assets/1. Scripts/Tailing/Follow/FollowManager.cs
+++ Assets/1. Scripts/Tailing/Follow/FollowManager.cs
@@ -103,12 +103,12 @@ public class FollowManager : MonoBehaviour
             UIManager.Instance.SetUI(eUIGameObjectName.DoubtGaugeSlider_Night, true);
             UIManager.Instance.SetUI(eUIGameObjectName.FatePositionSlider_Night, true);
             UIManager.Instance.SetUI(eUIGameObjectName.AccidyPositionSlider_Night, true);
-            // 방탈출2 UI 비활성화
-            UIManager.Instance.SetUI(eUIGameObjectName.ActionPoints, false);
-            UIManager.Instance.SetUI(eUIGameObjectName.MemoGauge, false);
-            UIManager.Instance.SetUI(eUIGameObjectName.LeftButton, false);
-            UIManager.Instance.SetUI(eUIGameObjectName.RightButton, false);
         }
+        // 방탈출 UI 비활성화
+        UIManager.Instance.SetUI(eUIGameObjectName.ActionPoints, false);
+        UIManager.Instance.SetUI(eUIGameObjectName.MemoGauge, false);
+        UIManager.Instance.SetUI(eUIGameObjectName.LeftButton, false);
+        UIManager.Instance.SetUI(eUIGameObjectName.RightButton, false);
     }
     public void TutorialNextStep(string step)
     {
