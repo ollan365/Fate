74c1b9828224535e7ca5e5f4f8c7390e5de31ecb
ollan365 <80192257+ollan365@users.noreply.github.com>
2025-12-08 00:09:33 +0900

    엔딩 연출 추가
    - 현재 Exit 버튼과 페이지 이동 버튼이 비활성화 후에도 계속 활성화되는 버그 존재
    
    - 터치 막는 함수 주석으로만 추가

--- Assets/1. Scripts/AlbumButton.cs
+++ Assets/1. Scripts/AlbumButton.cs
@@ -7,6 +7,8 @@ public class AlbumButton : MonoBehaviour
     public void OnMouseDown() {
         UIManager.Instance.SetUI(eUIGameObjectName.BlurImage, true, true);
         UIManager.Instance.SetUI(eUIGameObjectName.Album, true, true, FloatDirection.Up);
+        UIManager.Instance.SetUI(eUIGameObjectName.AlbumNextPageButton, true);
+        UIManager.Instance.SetUI(eUIGameObjectName.AlbumPreviousPageButton, true);
         UIManager.Instance.SetUI(eUIGameObjectName.ExitButton, true);
         gameObject.SetActive(false);
     }
